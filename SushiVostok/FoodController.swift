d6ef8440b02e52875a57ab1e184bc64d27848e89
Ed <esinyakov@baltbet.ru>
2020-05-20 19:23:34 +0300

    save

--- SushiVostok/FoodController.swift
+++ SushiVostok/FoodController.swift
@@ -13,15 +13,15 @@ class FoodController: UIViewController {
 
 	private let FoodCell = "FoodCell"
 
-	private let names = [""]
-	private let descriptions = [""]
-	private let prices: [Int] = [1]
-	private let images = [""]
+	private let names = ["КАМЕЛИЯ", "АЛЯСКА" ]
+	private let descriptions = ["Состав: нори, рис, омлет, жареная кожа семги,помидор, кунжут, сыр хохланд, икра масага", "Состав: Нори, Рис, Лосось, Лук, Огурец, Сыр, Кунжут"]
+	private let prices: [Int] = [220, 195, 189, 340, 195, 189, 195, 210, 133, 210, 195, 80, 195, 210]
+	private let images = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+		setupTableView()
+		print(prices.count)
     }
     
 
