8facbbad41f08766ac270ff1ce23519e338355dc
Ed <esinyakov@baltbet.ru>
2020-05-20 21:06:21 +0300

    update added

--- SushiVostok/FoodCell.swift
+++ SushiVostok/FoodCell.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class FoodCell: UITableViewCell {
-
+	@IBOutlet weak var itemImage: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var priceLabel: UILabel!
+	
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
 	func update(food: Food) {
-
+		itemImage.image = UIImage(named: food.image)
+		nameLabel.text = food.name
+		descriptionLabel.text = food.description
+		priceLabel.text = String(food.price)
 	}
 
 }
