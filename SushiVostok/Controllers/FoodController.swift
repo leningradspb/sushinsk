b693814ca2b62c1860f517daa3d90430f2ba292b
Ed <esinyakov@baltbet.ru>
2020-05-22 17:38:40 +0300

    save

--- SushiVostok/Controllers/FoodController.swift
+++ SushiVostok/Controllers/FoodController.swift
@@ -48,10 +48,6 @@ extension FoodController: UITableViewDataSource, UITableViewDelegate {
 		return cell
 	}
 
-	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		357
-	}
-
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		if let cell = tableView.cellForRow(at: indexPath) as? FoodCell {
 
